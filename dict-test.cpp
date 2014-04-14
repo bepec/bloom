dfed07e74f05c1a168823644553f12c933693a2f
Alex Vedmidsky <loafshock@gmail.com>
2014-04-14 12:04:57 +0300

    added manual test with words dictionary

---
+++ dict-test.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include "BloomFilter.hpp"
+#include "HashSetCalculatorMD5.hpp"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+   HashSetCalculatorMD5 hasher(3, 3);
+   BloomFilter filter(hasher);
+
+   assert(argc == 2);
+
+   ifstream fin(argv[1]);
+
+   string word;
+   int counter = 0;
+
+   while (fin.good())
+   {
+      fin >> word;
+      filter.feed(word);
+      ++counter;
+   }
+
+   cout << "Read words: " << counter << endl;
+   cout << "Please enter a word to check, empty line to finish." << endl;
+
+   do
+   {
+      cin >> word;
+      if (filter.has(word))
+      {
+         cout << "correct" << endl;
+      }
+      else
+      {
+         cout << "unknown" << endl;
+      }
+   } while (!word.empty());
+
+   return 0;
+}
---
+++ dict-test.sh
@@ -0,0 +1,1 @@
+g++ -o dict-test -l crypto -Wall HashSetCalculatorMD5.cpp BloomFilter.cpp dict-test.cpp && ./dict-test words
